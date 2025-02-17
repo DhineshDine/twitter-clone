434f428d75ae4078e7c1ebe5956b20778b88021d
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-02-17 12:51:23 +0530

    1-post routes and controllers added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -49,7 +49,14 @@ bio:{
 link:{
     type:String,
     default:"",
-}
+},
+likedPosts:[
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        default:[]
+    }
+],
 },{timestamps:true})
 
 const User = mongoose.model("User", userSchema);
