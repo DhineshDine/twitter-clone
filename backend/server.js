db9da76d71e2526e5eaeb76f18deaa3b9047f43b
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-02-26 15:43:22 +0530

    3-payload changed

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit:"5mb"}));
+app.use(express.json({limit:"20mb"}));
 //parse req.body and the limit should be small in order to prevent dos attack
 app.use(express.urlencoded({extended:true}));
 
