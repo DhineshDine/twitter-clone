400bc8eed3f57ffba847dcb1a9d4842a27b8d24e
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-02-27 14:52:40 +0530

    4- Prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -20,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"20mb"}));
 //parse req.body and the limit should be small in order to prevent dos attack
@@ -31,6 +32,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"/front-end/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"front-end","dist","index.html"));
+    })
+}
 
 
 app.listen(PORT, () =>{
